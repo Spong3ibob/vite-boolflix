7558e724eed00a28e9681319b741044aeccdf8a0
Spong3ibob <leonardomaran17@gmail.com>
2022-11-30 15:02:49 +0100

    creato struttura base vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from '../src/components/AppHeader.vue'
+import AppMain from '../src/components/AppMain.vue'
+
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+  
+}
+</script>
+
+<template>
+<AppHeader/>
+<AppMain/>
+</template>
+
+<style>
+@import url(./global.scss);
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+<h1>BOOLFLIX</h1>
+</template>
+
+<style>
+h1{
+    color: red;
+}
+</style>
